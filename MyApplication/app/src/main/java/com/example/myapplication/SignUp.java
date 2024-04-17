4e12a10fc3b3154077890246bd73b8148a3d6c9c
Jakub Brzuchacz <jakub_brzuchacz@o2.pl>
2024-04-17 21:42:59 +0200

    SignUp Updated

--- MyApplication/app/src/main/java/com/example/myapplication/SignUp.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SignUp.java
@@ -38,18 +38,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener{
-
-    private DatePickerDialog datePickerDialog;
-    private Button birthdayButton;
     private Button moveToLogin;
     private EditText nameEditText;
     private EditText surnameEditText;
     private EditText emailEditText;
-    private EditText phoneNumberEdit;
     private EditText passwordEdit1;
     private EditText passwordEdit2;
     private Button signUpButton;
-    private String selectedDate = "";
 
     private ProgressDialog progressDialog;
 
@@ -59,18 +54,12 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.signup);
-        // Inicjalizacja pól formularza
         nameEditText = findViewById(R.id.nameEditText);
         surnameEditText = findViewById(R.id.surnameEditText);
         emailEditText = findViewById(R.id.signUpEmailEdit);
-        phoneNumberEdit = findViewById(R.id.phoneNumberEdit);
         passwordEdit1 = findViewById(R.id.passwordEdit1);
         passwordEdit2 = findViewById(R.id.passwordEdit2);
         signUpButton = findViewById(R.id.signUpButton);
-
-        // Inicjalizacja przycisku birthdayButton
-        birthdayButton = findViewById(R.id.birthdateButton);
-        initDatePicker();
         signUpButton.setOnClickListener(this);
         progressDialog = new ProgressDialog(this);
 
@@ -84,59 +73,6 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
                 finish();
             }
         });
-    }
-
-    private String getTodaysDate() {
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        return makeDateString(day, month, year);
-    }
-
-    private void initDatePicker() {
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                month = month + 1;
-                selectedDate = makeDateString(day, month, year);
-                birthdayButton.setText(selectedDate);
-            }
-        };
-
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-
-        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
-    }
-
-    private String makeDateString(int day, int month, int year) {
-        return getMonthFormat(month) + " " + day + " " + year;
-    }
-
-    private String getMonthFormat(int month) {
-        if (month == 1) return "JAN";
-        else if(month == 2) return "FEB";
-        else if(month == 3) return "MAR";
-        else if(month == 4) return "APR";
-        else if(month == 5) return "MAY";
-        else if(month == 6) return "JUN";
-        else if(month == 7) return "JUL";
-        else if(month == 8) return "AUG";
-        else if(month == 9) return "SEP";
-        else if(month == 10) return "OCT";
-        else if(month == 11) return "NOV";
-        else if(month == 12) return "DEC";
-        return "JAN";
-    }
-
-    public void openDatePicker(View view) {
-        datePickerDialog.show();
     }
     public interface UserExistCallback {
         void onUserExist(boolean userExists);
@@ -179,12 +115,10 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         final String name = nameEditText.getText().toString();
         final String surname = surnameEditText.getText().toString();
         final String email = emailEditText.getText().toString();
-        final String birthdate = selectedDate;
-        final String phone = phoneNumberEdit.getText().toString();
         final String password1 = passwordEdit1.getText().toString();
         final String password2 = passwordEdit2.getText().toString();
 
-        if (name.isEmpty() || surname.isEmpty() || email.isEmpty() || birthdate.isEmpty() || phone.isEmpty() || password1.isEmpty() || password2.isEmpty()) {
+        if (name.isEmpty() || surname.isEmpty() || email.isEmpty() || password1.isEmpty() || password2.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Please complete all fields", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -241,8 +175,6 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
                             params.put("surname", surname);
                             params.put("email", email);
                             params.put("password", password1);
-                            params.put("birthdate", birthdate);
-                            params.put("phone", phone);
                             return params;
                         }
                     };
